67c0364335a4c6a2ce486a8004d58e0d089936b3
Teddyzxcv <pan2691712848@gmail.com>
2024-10-14 00:26:43 +0300

    dd ios project

---
+++ Mobile/LinguaCall/LinguaCall/LinguaCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinguaCallApp.swift
+//  LinguaCall
+//
+//  Created by Zhengwu Pan on 14.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct LinguaCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
